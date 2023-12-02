052e231bcadc8da9a933d4fcbd1222b2242cff06
carlin867 <140536486+carlin867@users.noreply.github.com>
2023-12-02 10:26:24 -0300

    atualizei

--- js.js
+++ js.js
@@ -1,13 +1,16 @@
 
     function menuShow() {
         var itens = document.querySelector('.itens');
-        var click = document.querySelector('.click')
+        var click = document.querySelector('.click');
+        var itenscheio = document.querySelector('.itens-cheio');
         if (itens.style.display == 'block') {
+          itenscheio.style.display = 'none';
           itens.style.display = 'none';
           click.style.background = 'green';
           document.querySelector('.click img').src = "open.svg";
           document.querySelector('.click:hover').src = "rgb(114, 168, 32)";
         } else {
+          itenscheio.style.display = 'none';
           itens.style.display = 'block';
           click.style.background = 'red';
           document.querySelector('.click img').src = "close.svg";
